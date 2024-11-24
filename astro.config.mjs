ea9366e8eb0a417c9128bac27e0ed847037c4cca
rb7a <130118715+rb7a@users.noreply.github.com>
2024-11-24 22:27:17 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 // import vercelServerless from "@astrojs/vercel/serverless";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
 });
